3d37f08e7e283a5b089e3fc459715fb645f86d30
Unknown <the.heart.of.paper@gmail.com>
2018-03-10 13:38:01 +0900

    現在地送信機能を実装

--- client/js/index.js
+++ client/js/index.js
@@ -3,5 +3,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
 import Application from "./components/Application";
+import { MuiThemeProvider } from "material-ui/styles";
 
-ReactDOM.render(<AppContainer><Application /></AppContainer>, document.getElementById("app"));
+//
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    // 登録成功
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // 登録失敗 :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+ReactDOM.render(
+  <AppContainer>
+    <MuiThemeProvider>
+      <Application />
+    </MuiThemeProvider>
+  </AppContainer>
+  , document.getElementById("app"));
